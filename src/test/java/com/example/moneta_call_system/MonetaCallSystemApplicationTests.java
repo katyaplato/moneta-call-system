894085330091b4a818e154bf5bdc21f235ee0396
Kateryna_Platonova <katyaplato@gmail.com>
2024-06-25 22:28:45 +0200

    Create a project

---
+++ src/test/java/com/example/moneta_call_system/MonetaCallSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moneta_call_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonetaCallSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
