d53e0da180c561367ffeade923a93dc316e8a381
Kateryna_Platonova <katyaplato@gmail.com>
2024-06-25 23:06:00 +0200

    Create a ticket repository

---
+++ src/main/java/com/example/moneta_call_system/TicketRepository.java
@@ -0,0 +1,9 @@
+package com.example.moneta_call_system;
+
+import com.example.moneta_call_system.models.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends JpaRepository <Ticket, Long> {
+}
