b314704bb2f6346200bbc503ac59ee99add60f73
Kateryna_Platonova <katyaplato@gmail.com>
2024-06-25 22:55:12 +0200

    Add basical endpoints

--- src/main/java/com/example/moneta_call_system/controllers/CallSystemController.java
+++ src/main/java/com/example/moneta_call_system/controllers/CallSystemController.java
@@ -1,8 +1,14 @@
 package com.example.moneta_call_system.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api")
 public interface CallSystemController {
-
+    @PostMapping("/create")
+    ResponseEntity<?> createTicket();
+    @DeleteMapping("/delete-last")
+    ResponseEntity<?> deleteLastTicket();
+    @GetMapping("/get-actual")
+    ResponseEntity<?> getActualTicket();
 }
