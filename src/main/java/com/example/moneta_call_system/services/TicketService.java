398bfce192278df82318267ce608f831d858846c
Kateryna_Platonova <katyaplato@gmail.com>
2024-06-25 23:08:38 +0200

    Add basic methods

--- src/main/java/com/example/moneta_call_system/services/TicketService.java
+++ src/main/java/com/example/moneta_call_system/services/TicketService.java
@@ -1,4 +1,9 @@
 package com.example.moneta_call_system.services;
 
+import com.example.moneta_call_system.models.Ticket;
+
 public interface TicketService {
+    Ticket createTicket();
+    void deleteLast();
+    Ticket getActiveTicket();
 }
