6943fd87ea62be218bb98ca791509980f67b3391
Kateryna_Platonova <katyaplato@gmail.com>
2024-06-25 22:51:14 +0200

    Create controller interface

---
+++ src/main/java/com/example/moneta_call_system/controllers/CallSystemController.java
@@ -0,0 +1,8 @@
+package com.example.moneta_call_system.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api")
+public interface CallSystemController {
+
+}
