6ec8d13905e6595f1246b8d9ad6d7ce2d2130fb9
Kateryna_Platonova <katyaplato@gmail.com>
2024-06-26 20:59:21 +0200

    complete generating endpoint

--- src/main/java/com/example/moneta_call_system/controllers/CallSystemControllerImpl.java
+++ src/main/java/com/example/moneta_call_system/controllers/CallSystemControllerImpl.java
@@ -1,15 +1,20 @@
 package com.example.moneta_call_system.controllers;
 
+import com.example.moneta_call_system.services.TicketService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class CallSystemControllerImpl implements CallSystemController {
+
+    TicketService ticketService;
+
     @Override
     public ResponseEntity<?> createTicket() {
-        return null;
+        return ResponseEntity.status(HttpStatus.CREATED).body(ticketService.createTicket());
     }
 
     @Override
