63d9e8e846cfcb1e622066eaa819a82cdb4bb317
Kateryna_Platonova <katyaplato@gmail.com>
2024-06-27 19:35:52 +0200

    make generate button functional

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const nextButton = document.getElementById("next");
+    const deleteButton = document.getElementById("delete");
+    const finishButton = document.getElementById("finish");
+    const statusMessage = document.getElementById("status-message");
+    const ticketNumber = document.getElementById("ticket-number")
+    const generateButton = document.getElementById("generate")
+
+    finishButton.style.display = "none";
+
+    generateButton.addEventListener("click", function () {
+        fetch("/api/create", {method: "POST"})
+            .then(response => response.json())
+            .then(data => {
+                statusMessage.textContent = `New ticket created with queue number ${data.queueNumber}.`;
+            });
+    });
+})
