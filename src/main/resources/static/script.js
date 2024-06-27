a79c1482eb8f180b729e506d436934e7098f1a9e
Kateryna_Platonova <katyaplato@gmail.com>
2024-06-27 19:55:21 +0200

    make next button functional

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -14,9 +14,41 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(data => {
                 statusMessage.textContent = `New ticket created with queue number ${data.queueNumber}.`;
                 setTimeout(() => {
-                    statusMessage.textContent = ""}, 5000)
+                    statusMessage.textContent = ""
+                }, 5000)
             });
     });
 
+    nextButton.addEventListener("click", function () {
+        fetch("/api/get-actual")
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error("The queue is empty.");
+                }
+            })
+            .then(data => {
+                nextButton.style.display = "none";
+                deleteButton.style.display = "none";
+                generateButton.style.display = "none";
+
+                ticketNumber.textContent = `Ticket Number: ${data.queueNumber}`;
+
+                const ticketDateTime = new Date(data.dateTime);
+                const formattedDate = ticketDateTime.toISOString().split('T')[0];
+                const formattedTime = ticketDateTime.toTimeString().split(' ')[0].slice(0, 5);
+
+                statusMessage.textContent = `An actual ticket was generated ${formattedDate} ${formattedTime}`;
+
+                finishButton.style.display = "block";
+            })
+            .catch(error => {
+                statusMessage.textContent = error.textContent = "The queue is empty.";
+                setTimeout(() => {
+                    statusMessage.textContent = error.textContent = "";
+                }, 5000);
+            });
+    });
 
 })
