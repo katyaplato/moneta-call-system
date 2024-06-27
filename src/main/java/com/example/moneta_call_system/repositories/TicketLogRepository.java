5c31dd25bef8f4ac3212b2c95333c57168f34e5c
Kateryna_Platonova <katyaplato@gmail.com>
2024-06-27 20:51:46 +0200

    refactor

--- src/main/java/com/example/moneta_call_system/repositories/TicketLogRepository.java
+++ src/main/java/com/example/moneta_call_system/repositories/TicketLogRepository.java
@@ -1,6 +1,5 @@
 package com.example.moneta_call_system.repositories;
 
-import com.example.moneta_call_system.models.Ticket;
 import com.example.moneta_call_system.models.TicketLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
